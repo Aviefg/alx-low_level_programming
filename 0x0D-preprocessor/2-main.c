7f5d7c8831ccea587f891080abaf3e130bb4f2fe
george <georgev421@gmail.com>
2023-04-19 01:12:37 +0100

    task 2 : file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function prints the name of file used to compile it
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
