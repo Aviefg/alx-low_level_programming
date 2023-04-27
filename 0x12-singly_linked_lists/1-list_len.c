5af81b247b3519c25937d15f2b175eb996616d2d
george <georgev421@gmail.com>
2023-04-27 18:07:35 +0100

    Task 1 - length of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - A function that returns the number of elements
+ * @h: A pointer to a linked list
+ * Return: len
+ */
+
 size_t list_len(const list_t *h)
 {
 	int len = 0;
